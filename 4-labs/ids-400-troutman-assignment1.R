8b5301d073de7eba43a10631ff98b9c7a3e72fea
Timothy Troutman <troutmantim@gmail.com>
2025-09-11 12:47:45 -0500

    Add lab files
    * Add Lab 1 R script and R Markdown files
    * Clean up and analyze Pokemon data using readr, dplyr, and janitor packages :smile:

---
+++ 4-labs/ids-400-troutman-assignment1.R
@@ -0,0 +1,12 @@
+### IDS 400 Lab Assignment 1 ###
+
+install.packages(c("rmarkdown", "knitr"))
+
+library(rmarkdown)
+library(knitr)
+
+getwd()
+
+setwd("C:/Users/trout/OneDrive/Documents/UIC MSBA/Fall 2025/IDS 400/Repo/ids400/4-labs")
+
+list.files()
